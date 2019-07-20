00cd2f2158aabe6bf5cf6af1c9d30c8d83c91703
Nivaldo Neto <nivaldojsneto@gmail.com>
2019-07-20 10:09:55 -0300

    translate select profile type label

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -168,7 +168,14 @@ module SearchHelper
     return '' if @templates[asset].blank? || @templates[asset].length == 1
     options = @templates[asset].map {|template| [template.name, template.id]}
     options = options_for_select([[_('Choose a template'), nil]] + options, selected: (params[:template_id]))
-    select_tag('template_id', options, :id => 'submenu')
+    select_tag('template_id', options, :id => 'templates-submenu')
+  end
+
+  def kinds_submenu(asset)
+    return '' if @kinds[asset].blank? || @kinds[asset].length == 1
+    options = @kinds[asset].map {|kind| [kind.name, kind.name]}
+    options = options_for_select([[_('Todos'), nil]] + options, selected: (params[:kind]))
+    select_tag('kind', options, :id => 'kinds-submenu')
   end
 
 end
