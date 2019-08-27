f879c4b20868a2d051a33614b5e06f038831392b
Nivaldo Neto <nivaldojsneto@gmail.com>
2019-08-27 14:37:49 -0300

    translater other

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -166,7 +166,7 @@ module ProfileHelper
   end
 
   def treat_sex(gender)
-    { 'male' => _('Male'), 'female' => _('Female'), 'other' => _('Outro') }[gender]
+    { 'male' => _('Male'), 'female' => _('Female'), 'other' => _('Other') }[gender]
   end
 
   def treat_date(date)
